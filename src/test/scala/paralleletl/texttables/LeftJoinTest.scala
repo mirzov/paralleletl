0f4a1eb2879ed2cfaaf540bddf9fb504fdc2275e
Oleg Mirzov <oleg.mirzov@gmail.com>
2013-07-17 14:53:39 +0300

    More tests for LeftJoin for TextTables

--- src/test/scala/paralleletl/texttables/LeftJoinTest.scala
+++ src/test/scala/paralleletl/texttables/LeftJoinTest.scala
@@ -43,4 +43,51 @@ b3	c3""")
 		
 		assert(await(actual) === await(expected))
 	}
+	
+	test("Duplicate right rows"){
+		val right = TsvDataTable("""B	C
+b1	c1
+b1	c2
+b2	c3""") 
+		val expected = TsvDataTable("""A	B	C
+a1	b1	c1
+a1	b1	c2
+a2	b2	c3""").getTsvString
+		
+		val actual = new LeftJoin(left, right, Seq("B"), Seq("B")).getTsvString
+		
+		assert(await(actual) === await(expected))
+	}
+	
+	test("Multi-column key"){
+		val left = TsvDataTable("""A	B	C
+a1	b1	c1
+a2	b2	c2""")
+		val right = TsvDataTable("""B	C	D
+b1	c1	d1
+b2	c2	d2""")
+		val expected = TsvDataTable("""A	B	C	D
+a1	b1	c1	d1
+a2	b2	c2	d2""").getTsvString
+
+		val actual = new LeftJoin(left, right, Seq("B", "C"), Seq("B", "C")).getTsvString
+		
+		assert(await(actual) === await(expected))
+	}
+	
+	test("Same-column conflict resolution"){
+		val left = TsvDataTable("""A	B	C
+a1	b1	c1_left
+a2	b2	c2_left""")
+		val right = TsvDataTable("""B	C	D
+b1	c1_right	d1
+b2	c2_right	d2""")
+		val expected = TsvDataTable("""A	B	C	D
+a1	b1	c1_left	d1
+a2	b2	c2_left	d2""").getTsvString
+
+		val actual = new LeftJoin(left, right, Seq("B"), Seq("B")).getTsvString
+		
+		assert(await(actual) === await(expected))
+	}
 }
